8cfb08e8fee5272d864f1bfc2239441b5891be40
zhangys11 <92197345+zhangys11@users.noreply.github.com>
2022-10-23 17:07:57 +0800

    Update calculate_recon_error.py

--- src/pyDRMetrics/calculate_recon_error.py
+++ src/pyDRMetrics/calculate_recon_error.py
@@ -2,26 +2,33 @@
 # Calculate the reconstruction error
 
 import numpy as np
-from numpy.linalg import norm
 
 def recon_MSE(X,Xr):
+    '''
+    Reconstruction relateive MSE
+    '''
     assert X.shape == Xr.shape
-    return norm(X-Xr, ord='fro')**2/(X.shape[0]*X.shape[1])
-
+    return np.linalg.norm(X-Xr, ord='fro')**2/(X.shape[0]*X.shape[1])
 
 def calculate_recon_error(X, Xr):
+    '''
+    Recontruction errors
+
+    Return
+    ------
+    mse : mean squared error between X and Xr
+    ams : mean square of X
+    mse/ams : relative mean squared error
+    '''
     assert X.shape == Xr.shape
 
     mse = recon_MSE(X, Xr) # mean square error
-    ms = recon_MSE(X, np.zeros(X.shape)) # mean square of original data matrix
+    ams = recon_MSE(X, np.zeros(X.shape)) # mean square of original data matrix
 
-    return mse, ms, mse/ms
+    return mse, ams, mse/ams
 
 def recon_rMSE(X, Xr):
+    '''
+    relative mean squared error
+    '''
     return recon_MSE(X, Xr) / recon_MSE(X, np.zeros(X.shape))
-
-def SNR(X, Xr):
-    '''
-    SNR (signal-Noise Ratio in dB). dB = 10*log10
-    '''
-    return 10 * np.log10(1.0 / recon_rMSE(X, Xr) )
